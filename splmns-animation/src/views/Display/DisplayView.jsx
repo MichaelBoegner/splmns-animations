8675c526b58911224d99bf741eaa6e6a9369222b
michaelboegner <cmikeb@gmail.com>
2025-07-10 15:24:56 +0700

    aspect ratio added

--- splmns-animation/src/views/Display/DisplayView.jsx
+++ splmns-animation/src/views/Display/DisplayView.jsx
@@ -4,15 +4,17 @@ import "./DisplayView.css";
 
 function DisplayView() {
   return (
-    <div className="display">
-      <div className="rain-overlay-front">
-        <RainOverlay />
-      </div>
-      <div className="character-spawner">
-        <CharacterSpawner />
-      </div>
-      <div className="rain-overlay-back">
-        <RainOverlay />
+    <div className="aspect-ratio">
+      <div className="display">
+        <div className="rain-overlay-front">
+          <RainOverlay />
+        </div>
+        <div className="character-spawner">
+          <CharacterSpawner />
+        </div>
+        <div className="rain-overlay-back">
+          <RainOverlay />
+        </div>
       </div>
     </div>
   );
